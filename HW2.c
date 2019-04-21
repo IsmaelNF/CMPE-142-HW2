f128235f4d8aaae2f545b36429ffcee223437a67
IsmaelNF <Ismael.navarro1998@gmail.com>
2019-04-21 00:37:50 -0700

    Working on parsing file

--- HW2.c
+++ HW2.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main(){
-	FILE *fopen(const char * jobs.dat, const char * r); //open file as read only
 
+	char ch, file_name[30];
+   	printf("Enter name of a file:\n");
+   	gets(file_name);
+ 
+   	FILE *fp = fopen(file_name, "r"); // open file in read mode
+ 
+   	if (fp == NULL) //throw handler in case file is empty
+   	{
+      		perror("Error opening the file.\n");
+      		exit(EXIT_FAILURE);
+   	}
+ 	
+	printf("The contents of %s file are:\n", file_name);
+	
+	//create arrays to parse data into
+	char id[100];
+	char arrival[100];
+	char duration[100];
+		
+   	while((ch = fgetc(fp)) != EOF){ //Read through file
+      		printf("%c", ch);
+		
+ 	}
 
-	int fclose(FILE *fp); //close file once done
+   	fclose(fp);
+   	return 0;
 }
